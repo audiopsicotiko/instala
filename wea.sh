8550c5e7fb751a2aed48dff65d13650c0c7f0f6a
audiopsicotiko <audiopsicotiko@gmail.com>
2019-05-14 22:41:36 -0400

    Update wea.sh

--- wea.sh
+++ wea.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
 # install dropbear
+echo "XKBMODEL="pc105"" /etc/default/keyboard
+echo "XKBLAYOUT="us"" /etc/default/keyboard
+echo "XKBVARIANT=""" /etc/default/keyboard
+echo "XKBOPTIONS=""" /etc/default/keyboard
+echo "BACKSPACE="guess"" /etc/default/keyboard
 apt-get -y install dropbear
